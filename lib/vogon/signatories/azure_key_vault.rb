f386302086e50c50527286aa42abf9a909385b91
Hayden Ball <hayden@playerdata.co.uk>
2019-09-26 11:30:05 +0100

    Allow AKV signer to accept a pre-generated access token

--- lib/vogon/signatories/azure_key_vault.rb
+++ lib/vogon/signatories/azure_key_vault.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ParameterLists
+
 require "httparty"
 
 module Vogon
@@ -9,12 +11,18 @@ module Vogon
 
       attr_accessor :base_url, :certificate_name, :tenant_id, :client_id, :client_secret
 
-      def initialize(base_url:, certificate_name:, tenant_id:, client_id:, client_secret:)
+      def initialize(
+        base_url:, certificate_name:, access_token: nil,
+        tenant_id: nil, client_id: nil, client_secret: nil
+      )
         self.base_url = base_url
         self.certificate_name = certificate_name
+
         self.tenant_id = tenant_id
         self.client_id = client_id
         self.client_secret = client_secret
+
+        @access_token = access_token
       end
 
       def ca_certificate
@@ -46,19 +54,23 @@ module Vogon
       private
 
       def access_token
-        @access_token ||= begin
-          response = self.class.post(
-            "https://login.microsoftonline.com/#{tenant_id}/oauth2/token",
-            multipart: true,
-            body: {
-              client_id: client_id, client_secret: client_secret,
-              grant_type: "client_credentials", resource: "https://vault.azure.net"
-            }
-          )
+        @access_token ||= fetch_access_token
+      end
 
-          JSON.parse(response.body)["access_token"]
-        end
+      def fetch_access_token
+        response = self.class.post(
+          "https://login.microsoftonline.com/#{tenant_id}/oauth2/token",
+          multipart: true,
+          body: {
+            client_id: client_id, client_secret: client_secret,
+            grant_type: "client_credentials", resource: "https://vault.azure.net"
+          }
+        )
+
+        JSON.parse(response.body)["access_token"]
       end
     end
   end
 end
+
+# rubocop:enable Metrics/ParameterLists
--- spec/signatories/azure_key_vault_spec.rb
+++ spec/signatories/azure_key_vault_spec.rb
@@ -5,36 +5,54 @@ require "base64"
 RSpec.describe Vogon::Signatories::AzureKeyVault, :vcr do
   let(:ca_cert) { OpenSSL::X509::Certificate.new File.read(fixture("ca.crt")) }
 
-  subject(:signatory) do
-    Vogon::Signatories::AzureKeyVault.new(
-      base_url: "https://vogon-development.vault.azure.net",
-      certificate_name: "vogon-test",
-      tenant_id: "73081428-e0d9-4468-ad4c-c89aec3a6f35",
-      client_id: ENV["AZURE_KEY_VAULT_CLIENT_ID"],
-      client_secret: ENV["AZURE_KEY_VAULT_CLIENT_SECRET"]
-    )
+  shared_examples "Vogon AzureKeyVault Signatory" do
+    it "fetches the CA certificate" do
+      expect(signatory.ca_certificate.subject.to_der).to eq ca_cert.subject.to_der
+    end
+
+    it "gets the issuer name" do
+      expect(signatory.issuer.to_der).to eq ca_cert.subject.to_der
+    end
+
+    it "signs a CSR" do
+      csr = Vogon::Containers::Request.new File.read(fixture("example.csr"))
+
+      expect(Base64.encode64(signatory.sign(csr))).to eq(
+        <<~SIGNATURE
+          AJguY4sRE8MCTR1mnnlPc2tWub/GyaNMcoc/vnPEcDiDLzVKaZALEef9RxxK
+          7031BjDOaljqgkKgeHjF1qyHoDgd5KlJyrYK4J53iTyLkkQNAhUgsVuHARsv
+          9xVbz5CBin3wNgY3JRIWMe2neU37n23k4CnOMUZUvLGuziSnPAXkyZFQmmCZ
+          h/UJMu04dtiHTbIziSBclJmKIRvAv/ksY+3OdC3kCklcojWTihTjmr2hSXW+
+          VW6GbZSMwmSWA6mZTDWIH4dWINpzv25jiFo1rJWEfDqdSLIJIxJil6ycJlFA
+          K1q0o1qAHq+GRJ20nSH1lc+7CxB27B8332IRrV0MeQ==
+        SIGNATURE
+      )
+    end
   end
 
-  it "fetches the CA certificate" do
-    expect(signatory.ca_certificate.subject.to_der).to eq ca_cert.subject.to_der
+  describe "using tenant_id, client_id, and client_secret to perform the OAuth dance" do
+    subject(:signatory) do
+      Vogon::Signatories::AzureKeyVault.new(
+        base_url: "https://vogon-development.vault.azure.net",
+        certificate_name: "vogon-test",
+        tenant_id: "73081428-e0d9-4468-ad4c-c89aec3a6f35",
+        client_id: ENV["AZURE_KEY_VAULT_CLIENT_ID"],
+        client_secret: ENV["AZURE_KEY_VAULT_CLIENT_SECRET"]
+      )
+    end
+
+    it_behaves_like "Vogon AzureKeyVault Signatory"
   end
 
-  it "gets the issuer name" do
-    expect(signatory.issuer.to_der).to eq ca_cert.subject.to_der
-  end
+  describe "using access_token" do
+    subject(:signatory) do
+      Vogon::Signatories::AzureKeyVault.new(
+        base_url: "https://vogon-development.vault.azure.net",
+        certificate_name: "vogon-test",
+        access_token: ENV["AZURE_KEY_VAULT_ACCESS_TOKEN"]
+      )
+    end
 
-  it "signs a CSR" do
-    csr = Vogon::Containers::Request.new File.read(fixture("example.csr"))
-
-    expect(Base64.encode64(signatory.sign(csr))).to eq(
-      <<~SIGNATURE
-        AJguY4sRE8MCTR1mnnlPc2tWub/GyaNMcoc/vnPEcDiDLzVKaZALEef9RxxK
-        7031BjDOaljqgkKgeHjF1qyHoDgd5KlJyrYK4J53iTyLkkQNAhUgsVuHARsv
-        9xVbz5CBin3wNgY3JRIWMe2neU37n23k4CnOMUZUvLGuziSnPAXkyZFQmmCZ
-        h/UJMu04dtiHTbIziSBclJmKIRvAv/ksY+3OdC3kCklcojWTihTjmr2hSXW+
-        VW6GbZSMwmSWA6mZTDWIH4dWINpzv25jiFo1rJWEfDqdSLIJIxJil6ycJlFA
-        K1q0o1qAHq+GRJ20nSH1lc+7CxB27B8332IRrV0MeQ==
-      SIGNATURE
-    )
+    it_behaves_like "Vogon AzureKeyVault Signatory"
   end
 end
